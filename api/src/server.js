d6a3f7a1a44da94d499cc1d40f5988b410d13f2c
Marcelo Santos Santa Rosa <marcelo.rosa@rizing.com>
2024-09-15 17:37:26 -0400

    Prepare to run both server and client on Heroku

--- api/src/server.js
+++ api/src/server.js
@@ -1,10 +1,19 @@
 import express from 'express';
 import {loggerHandler} from "./middleware/logger.js";
 import comicRouter from './routes/comic.routes.js';
+import path from 'path';
 
 const port = process.env.PORT || 3000;
 
+
 const app = express();
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../web/build')))
+
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '../web/build/index.html'))
+})
 
 app.use(loggerHandler);
 app.use(express.json());
