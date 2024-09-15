da40109eb70f9204632d1c2564ff2c2874160ce2
msantosifs <marcelo.rosa@academico.ifs.edu.br>
2024-09-15 18:18:24 -0400

    Fix route order to prevent 404 on /api/comic

--- api/src/server.js
+++ api/src/server.js
@@ -10,16 +10,18 @@ const __dirname = dirname(__filename);
 const port = process.env.PORT || 3000;
 const app = express();
 
+app.use(loggerHandler);
+app.use(express.json());
+
+// API routes
+app.use('/api/comic', comicRouter);
+
 // Serve static files from the React frontend app
 app.use(express.static(join(__dirname, '../../web/dist')));
 
-// AFTER defining routes: Anything that doesn't match, send back index.html
+// Catch-all route to serve index.html for any other requests
 app.get('*', (req, res) => {
     res.sendFile(join(__dirname, '../../web/dist/index.html'));
 });
-
-app.use(loggerHandler);
-app.use(express.json());
-app.use('/api/comic', comicRouter);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
