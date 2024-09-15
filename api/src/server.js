1203d98efa92cfcd600000c807a63612988165c8
msantosifs <marcelo.rosa@academico.ifs.edu.br>
2024-09-15 18:03:54 -0400

    Fix __dirname handling for ES modules

--- api/src/server.js
+++ api/src/server.js
@@ -1,24 +1,25 @@
 import express from 'express';
-import {loggerHandler} from "./middleware/logger.js";
-import comicRouter from './routes/comic.routes.js';
-import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { loggerHandler } from './middleware/logger.js'; // Use correct relative paths
+import comicRouter from './routes/comic.routes.js'; // Use correct relative paths
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const port = process.env.PORT || 3000;
-
-
 const app = express();
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '../web/build')))
 
-// AFTER defining routes: Anything that doesn't match what's above, send back index.html
+// Serve static files from the React frontend app
+app.use(express.static(join(__dirname, '../../web/build')));
+
+// AFTER defining routes: Anything that doesn't match, send back index.html
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '../web/build/index.html'))
-})
+    res.sendFile(join(__dirname, '../../web/build/index.html'));
+});
 
 app.use(loggerHandler);
 app.use(express.json());
 app.use('/api/comic', comicRouter);
 
-
-app.listen(port, () => console.log(`Listen to port ${port}`));
-
+app.listen(port, () => console.log(`Listening on port ${port}`));
